2710ad1aa83cb3e3cc7ed5ee263e01131f19897c
TobiasMaugus <tobiasmaugus705@gmail.com>
2025-11-13 08:43:30 -0300

    chore<setup>: inicializa projeto backend

---
+++ backend/ControleEstoqueVendas/src/test/java/backend/controleestoquevendas/ControleEstoqueVendasApplicationTests.java
@@ -0,0 +1,13 @@
+package backend.controleestoquevendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleEstoqueVendasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
