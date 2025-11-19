5503dc0a1de9680a7590a58c1bfdf63262988d7b
Kennedy <luis.turola@estudante.ufla.br>
2025-11-19 13:37:32 -0300

    Bug: Adequações no backend para aceitar request da web (fixes #43)

--- backend/ControleEstoqueVendas/src/main/java/backend/controleestoquevendas/repository/VendaProdutoRepository.java
+++
@@ -1,8 +0,0 @@
-package backend.controleestoquevendas.repository;
-
-import backend.controleestoquevendas.model.VendaProduto;
-import backend.controleestoquevendas.model.VendaProdutoId;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface VendaProdutoRepository extends JpaRepository<VendaProduto, VendaProdutoId> {
-}
