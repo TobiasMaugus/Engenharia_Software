03171d816a8d357f120d0ac8a56c62f0ac113d0c
Kennedy <luis.turola@estudante.ufla.br>
2025-11-25 17:30:36 -0300

    fix<EditarCliente.tsx>: Corrigido bug de request e adequado ao padrão definido (fixes #50)

--- ControleDeEstoqueVendas/frontEnd/src/Components/Pages/Cliente/EditarCliente.tsx
+++ ControleDeEstoqueVendas/frontEnd/src/Components/Pages/Cliente/EditarCliente.tsx
@@ -1,16 +1,29 @@
+import { useLocation } from "react-router-dom";
 import CadastroEdicaoForm from "../../CadastroEdicaoForm";
 
 export default function EditarCliente() {
-  return (
-    <CadastroEdicaoForm
-      titulo="Editar Cliente"
-      tipo="cliente"
-      modo="editar"
-      campos={[
-        { label: "Nome", name: "nome", type: "text" },
-        { label: "Telefone", name: "telefone", type: "text" },
-        { label: "Endereço", name: "endereco", type: "text" },
-      ]}
-    />
-  );
+    const location = useLocation();
+    const cliente = (location.state as any)?.cliente;
+
+    const dadosIniciais = cliente
+        ? {
+            nome: cliente.nome,
+            telefone: cliente.telefone,
+            cpf: cliente.cpf,
+        }
+        : {};
+
+    return (
+        <CadastroEdicaoForm
+            titulo="Editar Cliente"
+            tipo="cliente"
+            modo="editar"
+            campos={[
+                { label: "Nome", name: "nome", type: "text" },
+                { label: "Telefone", name: "telefone", type: "text" },
+                { label: "CPF", name: "cpf", type: "text" },
+            ]}
+            dadosIniciais={dadosIniciais}
+        />
+    );
 }
