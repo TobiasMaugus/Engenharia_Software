4efdd1b471a5d184adc38c0049ab999e3421a6de
Kennedy <luis.turola@estudante.ufla.br>
2025-11-28 10:11:18 -0300

    Refactor<ids>: Inserido ids em todos os campos para testes automatizados com selenium

--- ControleDeEstoqueVendas/frontEnd/src/Components/Header/Header.tsx
+++ ControleDeEstoqueVendas/frontEnd/src/Components/Header/Header.tsx
@@ -18,11 +18,12 @@ export default function Header() {
     return (
         <header className="flex justify-between items-center bg-[#2d3c3b] text-white px-6 py-3">
             <div className="flex items-center space-x-2">
-                <h1 className="font-bold text-lg">{user?.username}</h1>
-                <span className="text-[#94b1a0] font-semibold">{user?.role}</span>
+                <h1 id={"user-name"} className="font-bold text-lg">{user?.username}</h1>
+                <span id={"user-role"} className="text-[#94b1a0] font-semibold">{user?.role}</span>
             </div>
             <button
                 onClick={handleLogout}
+                id={"log-out-btn"}
                 className="bg-[#b45d44] text-white px-3 py-1 rounded-md hover:bg-[#c76a50] transition-colors"
             >
                 Log-out
